b1f4628a8a0eea7b7c985607f98344df85033dd8
Jesse Mallen <jmallen@campuslabs.com>
2017-02-07 11:56:58 -0500

    Initial commit

---
+++ src/NetCoreTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Newtonsoft.Json;
+
+namespace NetCoreTest
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				using (var client = new DocumentClient(
+					new Uri("https://jdm-docdb.documents.azure.com:443/"),
+					"YRnKNmv6qHHhyWFna6UK8sJG7eI7izeKenajyCQdqyGf0XNfjiwYtIWfL3lc1CyVywX0EbRasDlOaKZCBKAEhQ==",
+					new ConnectionPolicy
+					{
+						ConnectionMode = ConnectionMode.Direct,
+						ConnectionProtocol = Protocol.Tcp
+					}))
+				{
+					var query = client.CreateDocumentQuery(UriFactory.CreateCollectionUri("imports", "Items"),
+						"SELECT * FROM c where c.importId = \"d7968646-aec6-4687-be4f-e612cf361f76\"");
+					var docQuery = query.AsDocumentQuery();
+					GetResults(docQuery).Wait();
+					Debug.WriteLine("done");
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(e);
+			}
+		}
+
+		private static async Task GetResults(IDocumentQuery<dynamic> docQuery)
+		{
+			Debug.WriteLine("getting");
+			var results = await docQuery.ExecuteNextAsync();
+			Debug.WriteLine(JsonConvert.SerializeObject(results));
+		}
+	}
+}
